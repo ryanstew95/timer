4dea4117b33bc900c9ba0d6c600609988d93e3c7
Ryan Stewart <ryanstew17@gmail.com>
2023-05-11 13:03:54 -0300

    little beeping alarms

---
+++ timer1.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+  process.stdout.write('\x07');
+}, 3000);
+setTimeout(() => {
+  process.stdout.write('\x07');
+}, 5000);
+setTimeout(() => {
+  process.stdout.write('\x07');
+}, 9000);
+setTimeout(() => {
+  process.stdout.write('\x07');
+}, 10000);
+setTimeout(() => {
+  process.stdout.write('\x07');
+}, 15000);
