197d096da31b94e0e9df619e3008ae7320370af1
Ryan Stewart <ryanstew17@gmail.com>
2023-05-12 15:37:57 -0300

    a little alerm clock that beeps

--- timer1.js
+++ timer1.js
@@ -1,15 +1,20 @@
-setTimeout(() => {
-  process.stdout.write('\x07');
-}, 3000);
-setTimeout(() => {
-  process.stdout.write('\x07');
-}, 5000);
-setTimeout(() => {
-  process.stdout.write('\x07');
-}, 9000);
-setTimeout(() => {
-  process.stdout.write('\x07');
-}, 10000);
-setTimeout(() => {
-  process.stdout.write('\x07');
-}, 15000);
+const data = process.argv.slice(2);
+let numbers = data.filter(number => {
+  return number > 0;
+});
+
+const timer = function() {
+  for (const num of numbers) {
+    setTimeout(() => {
+      console.log('beep');
+      return process.stdout.write('\x07');
+    }, num * 1000);
+  }
+};
+
+timer(numbers);
+
+
+
+// Example usage:
+// node timer1.js 10 3 5 15 9
